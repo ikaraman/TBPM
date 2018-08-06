3b2d36b102de794f9d3b893b1a359786a830e175
Ivan Karaman <ikaraman@ya.ru>
2018-08-06 18:44:50 +1200

    Modified getFile function to fetch a file from a public s3 bucket

--- getFile.js
+++ getFile.js
@@ -1,3 +1,19 @@
+const request = require("request-promise-native");
+
 exports.getFile = function () {
-    return "test content";
+    return new Promise((resolve, reject) => {
+        const options = {
+            method: "GET",
+            uri: "https://s3-ap-southeast-2.amazonaws.com/tbpm-artifact-store/test.txt",
+            json: true,
+        };
+
+        request(options)
+            .then((result) => {
+                resolve(result);
+            })
+            .catch((error) => {
+                reject({ message: "Error happened while getting the file", error: error });
+            });
+    });
 };
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Nothing to see here!"));
 
 app.get("/getFile", (req, res) => {
-    const getFile = require("./getFile");
-    const fileContent = getFile();
-    res.send(fileContent);
+    return new Promise(() => {
+        const getter = require("./getFile");
+        getter.getFile()
+            .then((result) => {
+                res.send(result);
+            })
+            .catch((error) => {
+                res.send(error);
+            });
+    });
 });
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(3000, () => console.log("TBPM is listening on port 3000!"));
